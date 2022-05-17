4fbaa0e57834edc3502c5407b8d2463ee64722e0
joycenjichile <joycenkimalantoi@gmail.com>
2022-05-17 20:46:43 +0300

    calling functions

--- app/src/main/java/com/example/calculatoractivity/MainActivity.kt
+++ app/src/main/java/com/example/calculatoractivity/MainActivity.kt
@@ -5,22 +5,105 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import javax.security.auth.Subject
 
 class MainActivity : AppCompatActivity() {
-    lateinit var etNumberone:EditText
-    lateinit var etNumberTwo:EditText
-    lateinit var btnAdd:Button
-    lateinit var btnSubtract:Button
-    lateinit var btnMultiply:Button
-    lateinit var btnModulus:Button
+    lateinit var etNumberone: EditText
+    lateinit var etNumberTwo: EditText
+    lateinit var btnAdd: Button
+    lateinit var btnSubtract: Button
+    lateinit var btnMultiply: Button
+    lateinit var btnModulus: Button
+    lateinit var tvResults:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        etNumberone=findViewById(R.id.etNumberOne)
-        etNumberTwo=findViewById(R.id.etNumberTwo)
-        btnAdd=findViewById(R.id.btnAdd)
-        btnSubtract=findViewById(R.id.btnSubtract)
-        btnMultiply=findViewById(R.id.btnMultiply)
-        btnModulus=findViewById(R.id.btnModulus)
+        etNumberone = findViewById(R.id.etNumberOne)
+        etNumberTwo = findViewById(R.id.etNumberTwo)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnSubtract = findViewById(R.id.btnSubtract)
+        btnMultiply = findViewById(R.id.btnMultiply)
+        btnModulus = findViewById(R.id.btnModulus)
+        tvResults = findViewById(R.id.tvResults)
+
+        btnAdd.setOnClickListener {
+            val num1 = etNumberone.text.toString()
+            val num2 = etNumberTwo.text.toString()
+            if (num1.isBlank()){
+                etNumberone.error="Number is required"
+                return@setOnClickListener
+            }
+            if (num2.isBlank()){
+                etNumberTwo.error="Number is required"
+                return@setOnClickListener
+            }
+            addition(num1.toInt(), num2.toInt())
+        }
+
+        btnSubtract.setOnClickListener {
+            val num1 = etNumberone.text.toString()
+            val num2 = etNumberTwo.text.toString()
+            if (num1.isBlank()){
+                etNumberone.error="Number is required"
+                return@setOnClickListener
+            }
+            if (num2.isBlank()){
+                etNumberTwo.error="Number is required"
+                return@setOnClickListener
+            }
+            Subtract(num1.toInt(), num2.toInt())
+
+        }
+        btnMultiply.setOnClickListener {
+            val num1 = etNumberone.text.toString()
+            val num2 = etNumberTwo.text.toString()
+            if (num1.isBlank()){
+                etNumberone.error="Number is required"
+                return@setOnClickListener
+            }
+            if (num2.isBlank()){
+                etNumberTwo.error="Number is required"
+                return@setOnClickListener
+            }
+            Multiply(num1.toInt(), num2.toInt())
+
+        }
+        btnModulus.setOnClickListener {
+            val num1 = etNumberone.text.toString()
+            val num2 = etNumberTwo.text.toString()
+            if (num1.isBlank()){
+               etNumberone.error="Number is required"
+                return@setOnClickListener
+            }
+            if (num2.isBlank()){
+                etNumberTwo.error="Number is required"
+                return@setOnClickListener
+            }
+            Modulus(num1.toInt(), num2.toInt())
+
+        }
+    }
+
+    fun addition(num1: Int, num2: Int) {
+        var add = num1 + num2
+        tvResults.text = add.toString()
+
+    }
+
+    fun Subtract(num1: Int, num2: Int) {
+        var add = num1 + num2
+        tvResults.text = add.toString()
+
+
+    }
+
+    fun Multiply(num1: Int, num2: Int) {
+        var add = num1 + num2
+        tvResults.text = add.toString()
+    }
+
+    fun Modulus(num1: Int, num2: Int) {
+        var add = num1 + num2
+        tvResults.text = add.toString()
     }
 }
